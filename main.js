78e6e3929189b8d8ccb146174cc51ac9e63ae3cd
Olexandr <rybkolhoz@gmail.com>
2021-12-21 15:22:49 +0200

    first

---
+++ main.js
@@ -0,0 +1,34 @@
+const board =document.querySelector('#board')
+const squaresNumber = 300
+const colors =['red','yellow','purple',
+'green','orange','pink', 'blue','greenyellow']
+
+for(let i=0; i< squaresNumber; i++)
+{
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.addEventListener('mouseover', ()=>{
+        setColor(square)
+    })
+    square.addEventListener('mouseleave', ()=>{
+        removeColor(square)
+    })
+
+    board.append(square)
+}
+
+const setColor = (element)=>{
+    let color = randomColor()
+    element.style.backgroundColor = color;
+    element.style.boxShadow =`0 0 2px ${color}, 0 0 10px ${color}`;
+
+}
+const removeColor = (element)=>{
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow =`0 0 2px #000`;
+}
+const randomColor = ()=>{
+    const index = Math.floor(Math.random()* colors.length)
+    
+    return colors[index]
+}
